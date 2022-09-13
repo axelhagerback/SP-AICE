8534865f53aae3b1bfddefc1402cc52126817f46
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-13 15:31:59 +0200

    The person 1 div is now in the right place and has a border and text

--- public/aboutUs.js
+++ public/aboutUs.js
@@ -12,17 +12,24 @@ function buildAboutUs() {
 
 
     const topDiv = document.createElement('div');
-    topDiv.setAttribute('id', 'topdiv');
-    topDiv.className = '';
+    topDiv.className = 'w-75 mb-5 mx-auto pb-5';
     aboutUs.appendChild(topDiv);
 
     const person1Div = document.createElement('div');
-    person1Div.className = 'float-start';
+    person1Div.className = 'w-25 ml-3 border';
     topDiv.appendChild(person1Div);
 
     const p1Title = document.createElement('h2');
-    p1Title.className = 'float-start ml-2';
     p1Title.innerHTML = 'Axel';
-    topDiv.appendChild(p1Title);
+    p1Title.className = 'p-2';
+    person1Div.appendChild(p1Title);
+
+    const p1Text = document.createElement('p');
+    p1Text.className = 'p-2';
+    p1Text.innerHTML = 'sjdoasj isdoasjd jdisj kjsdk jl ksjdak jsdlj lj skldjaslj aslkd jk jk sjladkasjdasj ldkj ';
+    person1Div.appendChild(p1Text);
+
+
+
 
 };
