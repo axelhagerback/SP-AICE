632de280232e95a22f13db366f65bbebb363f06e
isacchr <isac.christiansson@gmail.com>
2022-09-13 15:03:27 +0200

    navbar finished and styled

--- public/navbar.js
+++ public/navbar.js
@@ -5,12 +5,24 @@ buildNavBar();
 function buildNavBar() {
     
     const divNavBar = document.getElementById('navBar');
-    divNavBar.className = 'border border-dark p-3 bg-primary d-flex sticky-top';
+    divNavBar.className = 'p-3 d-flex  sticky-top';
 
     const titleHome = document.createElement('h1');
-    titleHome.setAttribute('name', 'header');
+    titleHome.setAttribute('name', 'headerNavBar');
     titleHome.innerHTML = 'SP-AICE';
     titleHome.className = 'ms-4 w-25 float-start';
     divNavBar.appendChild(titleHome);
-    
+
+    const btnProjects = document.createElement('btn');
+    btnProjects.className = 'btn border-0';
+    btnProjects.setAttribute('id', 'btnProjects');
+    btnProjects.innerHTML = '<h1>Projects</h1>';
+    divNavBar.appendChild(btnProjects);
+
+    const btnEducation= document.createElement('btn');
+    btnEducation.setAttribute('id', 'btnEducation');
+    btnEducation.className = 'btn border-0';
+    btnEducation.innerHTML = '<h1>Education</h1>';
+    divNavBar.appendChild(btnEducation);
+
 };  
