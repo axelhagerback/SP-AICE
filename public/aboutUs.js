f5ded08abd70e8f44976fbf6b49f272d4f5e47d2
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-13 14:04:26 +0200

    added some divs to divide the web canvas into smaller pieces

--- public/aboutUs.js
+++ public/aboutUs.js
@@ -1,5 +1,24 @@
 console.log('hello from about us');
 
+buildAboutUs();
+
 function buildAboutUs() {
+
+    const topDiv = document.createElement('div');
+    topDiv.setAttribute('id', 'topdiv');
+    topDiv.className = '';
+
+    aboutUs.appendChild(topDiv);
+
+    const aboutUsTitleDiv = document.createElement('div');
+    aboutUsTitleDiv.className = '';
+    aboutUs.appendChild(aboutUsTitleDiv);
+
+    const aboutUsTitle = document.createElement('h1');
+    aboutUsTitle.className = '';
+    aboutUsTitle.innerHTML = 'About Us';
+    aboutUsTitleDiv.appendChild(aboutUsTitle);
+
+
 
 };
