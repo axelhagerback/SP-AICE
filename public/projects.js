fc77d234aae14da47ad5885c4052b580126c2050
elliotabrahamsson <elliotsson@outlook.com>
2022-09-13 14:11:11 +0200

    added project title

--- public/projects.js
+++ public/projects.js
@@ -1,5 +1,16 @@
 console.log('hello from projects');
 
-function buildProjects() {
+buildProjects()
 
+function buildProjects() {
+const Title = document.createElement('p');
+Title.className = 'text-center font-monospace fs-4';
+Title.innerHTML = 'Projects';
+projects.appendChild(Title);
+
+const BorderDiv = document.createElement('div');
+BorderDiv.className = 'container-sm bg-primary';
+projects.appendChild(BorderDiv);
+
+//const Border = document.createElement();
 };
