54ead53041649624fbbce91a94ff0a93958cec0e
elliotabrahamsson <elliotsson@outlook.com>
2022-09-14 13:58:14 +0200

    changed position of text in card div

--- public/projects.js
+++ public/projects.js
@@ -1,5 +1,3 @@
-console.log('hello from projects');
-
 buildProjects()
 
 function buildProjects() {
@@ -10,17 +8,17 @@ projects.appendChild(Title);
 
 const BorderDiv = document.createElement('div');
 BorderDiv.setAttribute('id', 'BorderDiv');
-BorderDiv.className = 'mx-auto card-body overflow-auto bg-primary w-50';
+BorderDiv.className = 'mx-auto rounded card-body bg-primary outline-primary w-50';
 projects.appendChild(BorderDiv);
 
 const ContentImage = document.createElement('img');
 ContentImage.setAttribute('src', 'project-design-in-project-management.png');
-ContentImage.className = 'card-img-top';
+ContentImage.className = 'card-img-top rounded';
 BorderDiv.appendChild(ContentImage);
 
 const ContentText = document.createElement('p');
-ContentText.className = 'text-center';
-ContentText.innerHTML = 'bhehfsdfjeiojfiwejsjrfisdfjiowehf ijiedfklgjrejigjdfjgkjerjgiodf wdasdwerkgjdfjgioqweuifhsdfughidjuij rj';
+ContentText.className = 'text-center w-75 ml-5 card-text';
+ContentText.innerHTML = 'bhehfsdfjeiojfiwejsjrfisdfjiowehf ijiedfklgjrejigjdfjgkjerjgiodf wdasdwerkgjdfjgioqweuifhsdfughidjuij rjreuithdfhgjdfgd jkhjdgkldfigjkl igjfgjhjsdjiowufis';
 BorderDiv.appendChild(ContentText); 
 
 
